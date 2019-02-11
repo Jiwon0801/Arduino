46f1a4f7e3a72f276e26b8bd7f711c2626035b78
Jiwon0801 <ssintico88@naver.com>
2019-02-11 17:58:28 +0900

    Library_Bluetooth

---
+++ Bluetooth/Bluetooth.h
@@ -0,0 +1,28 @@
+// Bluetooth.h
+
+#ifndef _BLUETOOTH_h
+#define _BLUETOOTH_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <SoftwareSerial.h>
+
+class Bluetooth : public SoftwareSerial
+{
+ protected:
+
+
+ public:
+	 Bluetooth(int rx, int tx, long boardrate);
+
+	 String readLine();
+};
+
+
+
+#endif
+
